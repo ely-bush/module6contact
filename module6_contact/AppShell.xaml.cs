41c32d04223a946034cf5bded81e8534c4d36e11
Ely <bushek@mail.uc.edu>
2026-06-22 22:49:30 -0400

    Add project files.

---
+++ module6_contact/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace module6_contact
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ module6_contact/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "module6_contact")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "module6_contact.Pages")]
